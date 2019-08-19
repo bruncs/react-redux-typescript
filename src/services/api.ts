3d5481b117f3540d180a9451bc49644581c8faca
Bruno Correia <brunocs@msn.com>
2019-08-19 08:47:32 -0300

    Add request interceptor with token

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+api.interceptors.request.use(async (configuration) => {
+  const token = await localStorage.getItem('token');
+  const config = configuration;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
